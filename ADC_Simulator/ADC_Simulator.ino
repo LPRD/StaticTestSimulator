2fbd468f051e700ba9e05f467c8f584712d44358
smit8297 <smit8297@umn.edu>
2017-02-04 11:50:03 -0600

    Added more temperature sensors, one at a larger range (0-1023 deg C)

---
+++ ADC_Simulator/ADC_Simulator.ino
@@ -0,0 +1,117 @@
+#define SCK 2
+#define DO 3
+#define CS1 4
+#define CS2 5
+#define CS3 6
+#define LCDO 8
+#define LoadcellPin A0
+#define TempPin1 A1
+#define TempPin2 A2
+#define TempPin3 A3
+
+int streamdata[32][3];
+long loadcelldata;
+
+int ipow(int base, int ex) // Power function for integers
+{
+    int base_o = base; // storing original base value
+    while(ex > 1) // multiplies base by itself while exponent is greater than one; if exponent equals one the function will return the base
+    {
+        base *= base_o;
+        ex--;
+    }
+    if(ex == 0) // returns 1 if exponent is zero (x^0 = 1 for all x)
+    {
+        return 1;
+    }
+    return base;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(DO, OUTPUT);
+  pinMode(LCDO, OUTPUT);
+  pinMode(CS1, INPUT);
+  pinMode(CS2, INPUT);
+  pinMode(CS3, INPUT);
+  pinMode(SCK, INPUT);
+  pinMode(LoadcellPin, INPUT);
+  pinMode(TempPin1, INPUT);
+  pinMode(TempPin2, INPUT);
+  pinMode(TempPin3, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+  for(int i = 0; i < 32; i++)
+  {
+    streamdata[i][0] = 0;
+    streamdata[i][1] = 0;
+    streamdata[i][2] = 0;
+  }
+  int tempdata[3] = {analogRead(TempPin1)/8,analogRead(TempPin2)/8,analogRead(TempPin3)};
+  for(int j = 0; j < 3; j++)
+  {
+      for(int i = 9; i >= 0; i--)
+      {
+        if(tempdata[j]%ipow(2,i+1) > 0 && ipow(2,i) <= tempdata[j])
+        {
+          streamdata[20+i][j] = 1;
+          tempdata[j] = tempdata[j]%ipow(2,i);
+        }
+        else
+        {
+          streamdata[20+i][j] = 0;
+        }
+      }
+  }
+
+  
+  /*
+  for(int i = 31; i >= 0; i--)
+  {
+    Serial.print(streamdata[i]);
+  }
+  Serial.println();
+  */
+  int i = 31;
+  while(digitalRead(CS1) == LOW && i >= 0)
+  {
+    delay(1);
+    if(digitalRead(SCK) == LOW)
+    {
+      digitalWrite(DO, streamdata[i][0]);
+      i--;
+      delay(1);
+      
+    }
+  }
+  int j = 31;
+  while(digitalRead(CS2) == LOW && j >= 0)
+  {
+    delay(1);
+    if(digitalRead(SCK) == LOW)
+    {
+      digitalWrite(DO, streamdata[j][1]);
+      j--;
+      delay(1);
+      
+    }
+  }
+  int k = 31;
+  while(digitalRead(CS3) == LOW && k >= 0)
+  {
+    delay(1);
+    if(digitalRead(SCK) == LOW)
+    {
+      digitalWrite(DO, streamdata[k][2]);
+      k--;
+      delay(1);
+      
+    }
+  }
+  //delay(250);
+  //Serial.println();
+  //Serial.println("New Data");
+}
---
+++ Force_and_Tempx3/Force_and_Tempx3.ino
@@ -0,0 +1,103 @@
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+
+#define CLK 2
+#define MAXDO 3
+#define MAXCS1 4 
+#define MAXCS2 5
+#define MAXCS3 6
+#define LCDO 8
+#define PRESSURE_PIN 10 
+#define PERIOD 100
+#define calibration_factor 20400
+
+Adafruit_MAX31855 thermocouple_1(MAXCLK, MAXCS1, MAXDO);
+Adafruit_MAX31855 thermocouple_2(MAXCLK, MAXCS2, MAXDO);
+Adafruit_MAX31855 thermocouple_3(MAXCLK, MAXCS3, MAXDO);
+
+HX711 loadcell(LCDO, CLK);
+
+unsigned long last_run_time = 0;
+
+
+void setup() 
+{
+  while (!Serial); // wait for Serial on Leonardo/Zero, etc
+  Serial.begin(9600);
+  pinMode(PRESSURE_PIN, INPUT);
+  pinMode(LCDO, INPUT);
+
+  loadcell.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  loadcell.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
+  
+  delay (500); // wait for MAX chip to stabilize 
+}
+
+void loop() 
+{
+  if (millis () > last_run_time + PERIOD) 
+  {
+    last_run_time = millis ();
+    /*Serial.print ("PSIG = ");
+    Serial.println (getPressure(analogRead(PRESSURE_PIN)));
+    //error message
+    */
+    if (isnan(getTemperature_1_Celsius()) || isnan(getTemperature_2_Celsius()) || isnan(getTemperature_3_Celsius()))
+    {
+      Serial.println ("Something wrong with thermocouple !");
+    }
+    else 
+    {
+      double CTemp = getTemperature_1_Celsius();
+      Serial.print("C_1= ");
+      Serial.println(CTemp); 
+      CTemp = getTemperature_2_Celsius();
+      Serial.print("C_2= ");
+      Serial.println(CTemp); 
+      CTemp = getTemperature_3_Celsius();
+      Serial.print("C_3= ");
+      Serial.println(CTemp); 
+      Serial.print("Force= ")
+      Serial.println(loadcell.get_units(),1)
+      //Serial.print ("F = ");
+      //Serial.println (CTemp * 1.8 + 32);    
+    }
+  }
+}
+
+double getPressure (double reading) 
+{
+  double PSIG = getVDC(reading) * 246.58 - 118.33;
+  return PSIG;
+}
+
+double getVDC (double reading) 
+{
+  double VDC = reading *5 / 1024;
+  return VDC;  
+}
+
+double getInternalTemperature () 
+{
+  return thermocouple_1.readInternal();
+}
+
+double getTemperature_1_Celsius() 
+{
+  return thermocouple_1.readCelsius();
+}
+
+double getTemperature_2_Celsius() 
+{
+  return thermocouple_2.readCelsius();
+}
+
+double getTemperature_3_Celsius() 
+{
+  return thermocouple_3.readCelsius();
+}
+
+
+
+ 
+
